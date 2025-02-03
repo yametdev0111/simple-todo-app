a3b9c40a41299f5807e0b005f141373d8beccb44
yametdev0111 <yametdev0111@gmail.com>
2025-02-03 16:08:14 -0500

    Styled and tested app

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Toolbar = ({ eliminate, clear, undo, redo }) => {
   return (
-    <div style={{ display: "flex", alignContent: "space-between" }}>
+    <div className="form-container" style={{ paddingTop: "10px" }}>
       <button onClick={eliminate}>Eliminate</button>
       <button onClick={clear}>Clear</button>
       <button onClick={undo}>Undo</button>
