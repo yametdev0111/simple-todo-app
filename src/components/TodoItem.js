bc3da083b145e0543128ea154501644b9ed2c4d8
yametdev0111 <yametdev0111@gmail.com>
2025-02-03 14:56:20 -0500

    Migrated: InputTodo, TodoItem, TodoList

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,28 +1,26 @@
 import React from "react";
 
-class TodoItem extends React.Component {
-  render() {
-    const completedStyle = {
-      fontStyle: "italic",
-      color: "#d35e0f",
-      opacity: 0.4,
-      textDecoration: "line-through"
-    };
+const completedStyle = {
+  fontStyle: "italic",
+  color: "#d35e0f",
+  opacity: 0.4,
+  textDecoration: "line-through"
+};
 
-    const { completed, id, title } = this.props.todo;
+const TodoItem = ({ todo, handleChangeProps, deleteTodoProps }) => {
+  const { completed, id, title } = todo;
 
-    return (
-      <li className="todo-item">
-        <input
-          type="checkbox"
-          checked={completed}
-          onChange={() => this.props.handleChangeProps(id)}
-        />
-        <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
-        <span style={completed ? completedStyle : null}>{title}</span>
-      </li>
-    );
-  }
+  return (
+    <li className="todo-item">
+      <input
+        type="checkbox"
+        checked={completed}
+        onChange={() => handleChangeProps(id)}
+      />
+      <button onClick={() => deleteTodoProps(id)}>Delete</button>
+      <span style={completed ? completedStyle : null}>{title}</span>
+    </li>
+  );
 }
 
 export default TodoItem;
